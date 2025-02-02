e10e5a33ec2cdce7c3d60e0b851a26ad082f7837
Kirill Ivanov <129665585+IKS26@users.noreply.github.com>
2025-02-02 18:32:52 +0300

    Added Rollbar integration (step 11.4)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/', // Важно для правильных путей!
+  resolve: {
+    alias: {
+      i18next: path.resolve(__dirname, 'node_modules/i18next')
+    }
+  },
   build: {
     outDir: 'dist', // Должно совпадать с тем, что мы раздаём
     rollupOptions: {
